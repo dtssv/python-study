2f3efce39567847cd437d5c4edaad495ca5cb923
SignleMR <709423445@qq.com>
2018-06-10 22:02:34 +0800

    python

--- www/Model.py
+++ www/Model.py
@@ -1,6 +1,12 @@
 import logging
 class Model(dict,metaclass=ModelMetaClass):
-    pass
+    def __init__(self,**kw):
+        super(Model,self).__init__(**kw)
+    def __getattr__(self, item):
+        try:
+            return self[item]
+        except KeyError:
+            raise AttributeError(r'Model object hash no key:%s' % item)
 class ModelMetaClass(type):
     def __new__(cls, name,bases,attrs):
         if name=='Model':
@@ -10,7 +16,35 @@ class ModelMetaClass(type):
         mapping = dict()
         fields = []
         key = None
-
+        for k,v in attrs.items():
+            if isinstance(v,Field):
+                logging.info('founding mapping %s:%s' %(k,v))
+                mapping[k] = v
+                if v.key:
+                    if key:
+                        raise BaseException('Duplicate primary key for field: %s' % k)
+                    key = k
+                else:
+                    fields.append(k)
+        if not key:
+            raise BaseException('Primary key not found')
+        for k in mapping.keys():
+            attrs.pop(k)
+        escapeFields = list(map(lambda f:'`%s`' % f,fields))
+        attrs['mapping'] = mapping
+        attrs['fields'] = fields
+        attrs['key'] = key
+        attrs['tableName'] = tableName
+        attrs['select'] = 'select %s,%s from %s' %(key,', '.join(fields),tableName)
+        attrs['insert'] = 'insert into %s (%s,%s) values (%s)' %(tableName,key, ', '.join(escapeFields),createArgsString(len(escapeFields)+1))
+        attrs['update'] = 'update %s set %s where %s=?' %(tableName,', '.join(map(lambda f:'`%s`=?' % mapping.get(f).name or f,fields)),key)
+        attrs['delete'] = 'delete from %s where %s=?' %(tableName,key)
+        return type.__new__(cls,name,bases,attrs)
+def createArgsString(n):
+    array = []
+    for i in range(n):
+        array.append('?')
+    return ', '.join(array)
 class Field(object):
     def __init__(self,name,type,key,default):
         self.name = name
